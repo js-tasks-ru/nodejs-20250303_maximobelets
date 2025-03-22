d03ffa0df0f99b9fbecc238cc1693a28e3a3fdd5
maximobelets <maximobelets@gmail.com>
2025-03-22 22:12:01 +0500

    Add solution for 3 module 1 task

--- 03-di/01-notification-service/notifications/notifications.service.ts
+++ 03-di/01-notification-service/notifications/notifications.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 
 @Injectable()
-export class NotificationsService {}
+export class NotificationsService {
+    sendEmail(to: string, subject: string, message: string): void {
+        if (!to) throw new BadRequestException('User not found')
+
+        if (!subject || !message) throw new BadRequestException('Incorrect data')
+
+        const emailNotification: string = `Email sent to ${to}: [${subject}] ${message}`;
+
+        return console.log(emailNotification);
+    }
+
+    sendSMS(to: string, message: string): void {
+        if (!to) throw new BadRequestException('User not found')
+        
+        if (!message) throw new BadRequestException('Incorrect message')
+
+        const smsNotification: string = `SMS sent to ${to}: ${message}`;
+
+        return console.log(smsNotification);
+    }
+}

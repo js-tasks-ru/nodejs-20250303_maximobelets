18578be76c51058779e8cb41eecc221e7a744636
maximobelets <maximobelets@gmail.com>
2025-03-27 17:08:01 +0500

    Add solution for 4 module 1 task

--- 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
+++ 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
@@ -1,5 +1,28 @@
 import { ArgumentsHost, ExceptionFilter } from "@nestjs/common";
+import { HttpArgumentsHost } from "@nestjs/common/interfaces";
+import { Response } from 'express';
+
+import * as fs from "fs";
 
 export class HttpErrorFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {}
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx: HttpArgumentsHost = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.status || 500;
+    const timestamp: string = new Date().toISOString();
+
+    const message: string = `[${timestamp}] ${status} - ${exception.message}\n`;
+
+    try {
+      fs.appendFileSync('errors.log', message);
+    } catch {
+      console.log('Something went wrong with the log process');
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      message: exception.message,
+      timestamp: timestamp,
+    });
+  }
 }
--- 04-request-lifecycle/01-nestjs-components/pipes/parse-int.pipe.ts
+++ 04-request-lifecycle/01-nestjs-components/pipes/parse-int.pipe.ts
@@ -1,5 +1,11 @@
-import { PipeTransform } from "@nestjs/common";
+import { BadRequestException, PipeTransform } from "@nestjs/common";
 
 export class ParseIntPipe implements PipeTransform {
-  transform(value: string): number {}
+  transform(value: string): number {
+    const id: number = parseInt(value, 10);
+    
+    if (isNaN(id)) throw new BadRequestException(`"${value}" не является числом`);
+
+    return id;
+  }
 }
